cc439bffd0c991e50a9dd510c2f3932cdad49901
David MacKenzie <dmackenzie1@gmail.com>
2020-12-06 09:15:08 -0600

    hottub.js interface

---
+++ public/client.js
@@ -0,0 +1,14 @@
+hottubclient={
+	initialize: function(){
+		console.log("ready")
+	},
+	getTemperature: function(){
+	}
+};
+
+
+
+$( document ).ready(function() {
+    hottubclient.initialize();
+});
+
